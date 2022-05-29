204ab615df00d227d7afaed75a4f9ed6720b6d4d
jeungHunLee <ksjs1111@gmail.com>
2022-05-29 22:14:37 +0900

    Array List Queue

--- CollectionAndFramework/src/collection/arraylist/QueueArrayListTest.java
+++ CollectionAndFramework/src/collection/arraylist/QueueArrayListTest.java
@@ -1,2 +1,38 @@
-package collection.arraylist;public class QueueArrayListTest {
+package collection.arraylist;
+
+import java.util.ArrayList;
+
+class MyQueue {
+    ArrayList<Integer> myQueue = new ArrayList<Integer>();
+
+    //큐 요소 저장
+    public void enQueue(Integer data) {
+        myQueue.add(data);
+    }
+
+    //큐 요소 추출
+    public Integer deQueue() {
+        int len = myQueue.size();
+        if(len == 0) {
+            System.out.println("큐가 비었습니다.");
+        }
+
+        return myQueue.remove(0);
+    }
+}
+
+public class QueueArrayListTest {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+
+        myQueue.enQueue(1);
+        myQueue.enQueue(2);
+        myQueue.enQueue(3);
+        myQueue.enQueue(4);
+
+        System.out.println(myQueue.deQueue());
+        System.out.println(myQueue.deQueue());
+        System.out.println(myQueue.deQueue());
+        System.out.println(myQueue.deQueue());
+    }
 }
